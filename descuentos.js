c46cee774eab9fd406c91169cd9194d5976502c4
jhoz <henoczamora22@gmail.com>
2022-05-29 13:13:57 -0500

    Solucion 2 reto descuentos

--- descuentos.js
+++ descuentos.js
@@ -23,18 +23,19 @@ function onClickButtonPriceDiscount(){
     const couponValue = inputCoupon.value;
 
     let descuento;
-    
-    switch(couponValue) {
-      case coupons[0]: // "JuanDC_es_Batman"
-        descuento = 15;
-      break;
-      case coupons[1]: // "pero_no_le_digas_a_nadie"
-        descuento = 30;
-      break;
-      case coupons[2]: // "es_un_secreto"
-        descuento = 25;
-      break;
+    if (!coupons.includes(couponValue)) {
+        alert("El cupón " + couponValue + " no es válido");
     }
+
+    if (!coupons.includes(couponValue)) {
+        alert("El cupón " + couponValue + "no es válido");
+     } else if (couponValue === "JuanDC_es_Batman") {
+        descuento = 15;
+     } else if (couponValue === "pero_no_le_digas_a_nadie") {
+        descuento = 30;
+     } else if (couponValue === "es_un_secreto") {
+        descuento = 25;
+     }
     /* Para obtener el valor del select */
     const cod = parseInt(document.getElementById("cupons").value);
     const precioConDescuento =  calcularPrecioConDescuento(priceValue,descuento,cod);
