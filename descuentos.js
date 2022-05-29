6240641a69637436edc373164db173331527f4dc
jhoz <henoczamora22@gmail.com>
2022-05-29 13:46:52 -0500

    Solucion 3 y la mas optima creo

--- descuentos.js
+++ descuentos.js
@@ -9,45 +9,48 @@ function calcularPrecioConDescuento(precio,descuento ,cupones){
     return precioConDescuento
 }
 const coupons = [
-    "JuanDC_es_Batman",
-    "pero_no_le_digas_a_nadie",
-    "es_un_secreto",
+    {
+        name: "JuanDC_es_Batman",
+        discount: 15,
+    },
+    {
+        name: "pero_no_le_digas_a_nadie",
+        discount: 30,
+    },
+    {
+        name: "es_un_secreto",
+        discount: 25,
+    },
 ];
 function onClickButtonPriceDiscount(){
+    //se obtienen las variables que ingresan el los inputs
     const inputPrice = document.getElementById("InputPrice");
     const priceValue =  parseInt(inputPrice.value);
     
-    // const inputDiscount = document.getElementById("InputDiscount");
-    // const discountValue  =  parseInt(inputDiscount.value);
     const inputCoupon = document.getElementById("InputCoupon");
     const couponValue = inputCoupon.value;
 
-    let descuento;
-    if (!coupons.includes(couponValue)) {
-        alert("El cupón " + couponValue + " no es válido");
+//retorna un boolean, true si el valor el input coupoValue, se encuentra en el array con coupon.name
+    const isCouponValueValid = function (coupon){
+        return coupon.name === couponValue;
+    }
+//se usa el find en el array de cupones, y se pasa la funcion que valida si   
+//su parámetro coupon es igual a nuestra variable userCouponValue
+    const userCoupon = coupons.find(isCouponValueValid);
+//if para comprobar y mandar a notificar si el cupon es valido
+    if (!userCoupon) {
+        alert("El cupón " + couponValue + "no es válido");
+    } else {
+        //si resulta ser valido, se hace los calculos correspondientes y se manda la respuesta
+        const descuento = userCoupon.discount;
+        //cupones del combobox o select 
+        const cod = parseInt(document.getElementById("cupons").value);
+        const precioConDescuento =  calcularPrecioConDescuento(priceValue,descuento,cod);
+        const result1 = document.getElementById("Result1");
+        const result2 = document.getElementById("Result2");
+        result1.innerText = "El precio con descuento son: $" + precioConDescuento 
+        result2.innerText = "El dinero ahorrado es de: $"+ (priceValue-precioConDescuento).toFixed(2);
     }
 
-    if (!coupons.includes(couponValue)) {
-        alert("El cupón " + couponValue + "no es válido");
-     } else if (couponValue === "JuanDC_es_Batman") {
-        descuento = 15;
-     } else if (couponValue === "pero_no_le_digas_a_nadie") {
-        descuento = 30;
-     } else if (couponValue === "es_un_secreto") {
-        descuento = 25;
-     }
-    /* Para obtener el valor del select */
-    const cod = parseInt(document.getElementById("cupons").value);
-    const precioConDescuento =  calcularPrecioConDescuento(priceValue,descuento,cod);
-    const result1 = document.getElementById("Result1");
-    const result2 = document.getElementById("Result2");
-    result1.innerText = "El precio con descuento son: $" + precioConDescuento 
-    result2.innerText = "El dinero ahorrado es de: $"+ (priceValue-precioConDescuento).toFixed(2);
-        // +"El dinero ahorra es de: " +priceValue-precioConDescuento
+
 }
-// console.log({
-//     precioOriginal,
-//     descuento,
-//     porcentajePrecioConDescuento,
-//     precioConDescuento,
-// });
