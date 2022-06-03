1f97b527df65ec6945cf7e0254f82a76d35e08ad
jhoz <henoczamora22@gmail.com>
2022-06-02 23:26:54 -0500

    Reto terminao, me chorie el codigo porque No me costo hacer que el split funcionara, sigo sin entender el map reduce y filter, pero poco a poco

--- taller3.js
+++ taller3.js
@@ -6,77 +6,99 @@ input.addEventListener('keyup', validar);
 function validar(event){
     input.style.backgroundColor = "#488ba0";
 }
-function conversorArray(stringsDeNumeros){
-    if(stringsDeNumeros.includes(",")){
-        alert("Los valores no estan separados por comas (,)")
-        return;
-    }
-    let array = stringsDeNumeros.split(',');
-    let arrayNumber = parseInt(array);
-    
-    return arrayNumber
-}
-function calcularMedia(input){
-    var lista=conversorArray(input);
-    const sumaLista = lista.reduce(
-        function (valorAcumulado = 0, nuevoElement ){
-            return valorAcumulado + nuevoElement;   
-        }
-    );
-    const promedioLista = sumaLista/lista.length;
-    return promedioLista
+// function conversorArray(stringsDeNumeros){
+//     if(stringsDeNumeros.indexOf(", ") !== -1){
+
+
+//         var array =parseInt(stringsDeNumeros.split(', '));
+//         alert(array)
+//         return parseInt(stringsDeNumeros.split(', '));
+//     }else{
+//         alert("Los valores no estan separados por comas y espacio (', ')")
+//         return;    
+//     }
+        
+
+// }
+const calcularMedia = (iterable) => {
+    let array = iterable.split(", ");
+    let suma_elementos = array.reduce((elemento1, elemento2) => Number(elemento1) + Number(elemento2))
+    return promedio = suma_elementos / array.length;
 }
 
-function calcularMediana (input){
-    // console.log(listaDesordenada)
-    var listaDesordenada=conversorArray(input);
-    const lista=listaDesordenada.sort(function (a,b){return a-b;})
-    console.log(lista)
-    const mitadLista = parseInt(lista.length/2);
-    let mediana;
-    if (esPar(lista.length)){
-        const elemento1 = lista[mitadLista-1];
-        const elemento2 = lista[mitadLista];
-        const promedioElemento1y2 = calcularMedia([
-            elemento1,
-            elemento2,
-        ]);
-        console.log("Elemento1:" +elemento1 + " Elemento2:" +elemento2)
-        mediana = promedioElemento1y2;
-    }else{
-        mediana = lista[mitadLista];
+function calcularMediana(iterable){
+    let array = iterable.split(", ");
+    
+    let lista_en_orden = array.sort((a, b) => a - b);
+    
+    let mitad_lista = parseInt(lista_en_orden.length / 2);
+    let mediana; 
+
+    if(lista_en_orden.length % 2 == 0){
+        let elemento = lista_en_orden[mitad_lista];
+        let elemento2 = lista_en_orden[mitad_lista - 1];
+        mediana = (Number(elemento) + Number(elemento2)) / 2;
     }
+    else {
+        mediana = lista_en_orden[mitad_lista];
+    }  
     return mediana
 }
-function calcularModa(input){
-    var lista=conversorArray(input);
-    const listaCount = {};
+
+
     
-    lista.map(
-        function (elemento){
-            if(listaCount[elemento]){
-                listaCount[elemento] +=1; 
-            }else{
-                listaCount[elemento] = 1;
+
+    const caulcularModa = (iterable) => {
+        const repeticion_numeros = {};
+        let array ;
+        array = iterable.split(", ")
+        let count_elements = array.map(elemento => {
+            if(repeticion_numeros[elemento]){
+                repeticion_numeros[elemento] += 1;
+            } 
+            else {
+                repeticion_numeros[elemento] = 1
+            }
+        }) 
+        count_elements = Object.entries(repeticion_numeros);
+        console.log(count_elements)
+        let menorAmayor = count_elements.sort((a, b) => a[1] - b[1])
+        /* let moda = menorAmayor[menorAmayor.length - 1]
+        console.log(moda) */
+        const modas = [];
+        for(let i = 0; i < menorAmayor.length; i++){
+            if(menorAmayor[i][1] == menorAmayor[menorAmayor.length - 1][1]){
+                modas.push(menorAmayor[i])
             }
         }
-    );
+        return modas
     
-    const listArray = Object.entries(listaCount).sort(
-        function (a,b){
-            return a[1]-b[1];
-        }
-    )
-    
-    const moda = listArray[listArray.length-1]
-    return moda
+    } 
+function calcularMediajs(){
+    const inputString =  document.getElementById("datos")
+    let average_value = inputString.value;
+    let resultado = calcularMedia(average_value);
+    pResultMedia.innerText = `Promedio: ${resultado.toFixed(2)}`
 }
-
 function calcularModajs(){
     const inputString =  document.getElementById("datos")
-    const moda = calcularModa(inputString.value);
-    const result = document.getElementById("pResultModa");
-    result.innerText = "La moda calculada es: $" + moda 
+    let moda_value = inputString.value;
+    let result = caulcularModa(moda_value);
+    if(result.length > 1) {
+        for(let i = 0; i < result.length; i++){
+            pResultModa.innerText =`La moda es ${result[i][0]} y se repite ${result[i][1]} veces`;
+        }
+    }
+    else 
+    {
+        pResultModa.innerText = `La moda es ${result[0][0]} se repite ${result[0][1]} veces`
+    }
+}
+function calcularMedianajs(){
+    const inputString =  document.getElementById("datos")
+    let median_value = inputString.value;
+    let result = calcularMediana(median_value);
+    pResultMediana.innerText  = `Mediana: ${result}`;
 }
 
 
